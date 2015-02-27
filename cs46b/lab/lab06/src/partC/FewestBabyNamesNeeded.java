5524478d2e92c052da3fc1ec167859a9867be8eb
Scot Matson <sjpmatson@gmail.com>
2015-02-27 07:52:40 -0800

    Lab 6 parts A, B, and D have been completed.

--- cs46b/lab/lab06/src/partC/FewestBabyNamesNeeded.java
+++ cs46b/lab/lab06/src/partC/FewestBabyNamesNeeded.java
@@ -17,19 +17,54 @@ public class FewestBabyNamesNeeded
         else {
             String filename = args[0];
             try {
-                File file = new File(filename);
-                Scanner in = new Scanner(file);
+                File inputFile = new File(filename); // get the file
+                Scanner in = new Scanner(inputFile, "UTF-8"); // feed the contents
 
+                // parse the data
                 if (args[1].equalsIgnoreCase("male")) {
-                    // Parse data, odd number strings are males, sum total count
+                    int manCounter = 0;
+                    double percentSum = 0.0;
+
+                    // Consuming data to access desired field. Probably not the right way to be doing this...
+                    while (in.hasNext() && percentSum < Double.parseDouble(args[2])) {
+                        in.next();
+                        in.next();
+                        in.next();
+                        percentSum += Double.parseDouble(in.next());
+                        in.nextLine();
+                        ++manCounter;
+                    }
+
+                    System.out.println(manCounter);
+
                 }
                 else if (args[1].equalsIgnoreCase("female")) { // if arg[2] is female
                     // Parse data, even number strings are female, sum total count
+                    int womanCounter = 0;
+                    double percentSum = 0.0;
+
+                    // This is stupidly verbose but I'm not sure how else to do it.
+                    while (in.hasNext() && percentSum < Double.parseDouble(args[2])) {
+                        // Again, consuming data.
+                        in.next();
+                        in.next();
+                        in.next();
+                        in.next();
+                        in.next();
+                        in.next();
+                        percentSum += Double.parseDouble(in.next());
+                        in.nextLine();
+                        ++womanCounter;
+                    }
+
+                    System.out.println(womanCounter);
                 }
 
                 in.close();
             } catch (FileNotFoundException e) {
                 System.out.println("File not found:  (" + filename + ")");
+                String curDir = System.getProperty("user.dir");
+                System.out.println(curDir);
             }
         }
     }
