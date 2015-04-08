e93badc523ef4a5397c5044516964bfe9b10be75
Scot Matson <sjpmatson@gmail.com>
2015-04-07 23:51:54 -0700

    Completed hw05 final and some lab incomplete lab files.

--- cs46b/homework/hw05final/src/Sorter.java
+++ cs46b/homework/hw05final/src/Sorter.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class Sorter
@@ -52,9 +51,23 @@ public class Sorter
      *
      * @param courses the array of courses to sort
      */
-    public static void selectionSortCourses(Course[] courses)
-    {
-        // YOUR CODE HERE
+    public static void selectionSortCourses(Course[] courses) {
+        for (int i = 0; i < courses.length - 1; ++i) {
+            int min = i;
+            for (int j = i + 1; j < courses.length; ++j) {
+                if (courses[j].size() < courses[min].size()) {
+                    min = j;
+                }
+            }
+
+            // The the current min has changed from the initial min...
+            if (min != i) {
+                // Swap
+                Course temp = courses[min];
+                courses[min] = courses[i];
+                courses[i] = temp;
+            }
+        }
     }
 
     /**
@@ -68,7 +81,58 @@ public class Sorter
      */
     public static Student[] mergeSortStudents(Student[] students)
     {
-        // YOUR CODE HERE
+        // Modified code from Big Java by Cai Horstmann
+        // Sizing the new array halves
+        if (students.length <= 1) { return students; }
+        Student[] first = new Student[students.length / 2];
+        Student[] second = new Student[students.length - first.length];
+
+        // Copying the original array elements into the new array
+        for (int i = 0; i < first.length; ++i) {
+            first[i] = students[i];
+        }
+        for (int i = 0; i < second.length; ++i) {
+            second[i] = students[first.length + i];
+        }
+
+        // Recursively continue this process until we have broken the original array
+        //  down into its constituents
+        mergeSortStudents(first);
+        mergeSortStudents(second);
+
+        // Begin merging to rebuild a sorted array
+        int iFirst = 0;     // Next element to consider in the first array
+        int iSecond = 0;    // Next element to consider int the second array
+        int j = 0;          // Next open position in students
+
+        // As long as we have not exceeded our counters have moved out of
+        //  bounds, continue to move the smallest element of the arrays
+        while (iFirst < first.length && iSecond < second.length) {
+            if (first[iFirst].getLastName().compareTo(second[iSecond].getLastName()) < 0) {
+                students[j] = first[iFirst];
+                ++iFirst;
+            }
+            else {
+                students[j] = second[iSecond];
+                ++iSecond;
+            }
+            ++j;
+        }
+
+        // Only one of the loops below will be used to clean up any
+        //  remaining array elements
+        while (iFirst < first.length) {
+            students[j] = first[iFirst];
+            ++iFirst;
+            ++j;
+        }
+        while (iSecond < second.length) {
+            students[j] = second[iSecond];
+            ++iSecond;
+            ++j;
+        }
+
+        return students;
     }
 
     /**
@@ -83,6 +147,25 @@ public class Sorter
      */
     public static int binarySearchStudents(String lastName, Student[] students)
     {
-        // YOUR CODE HERE
+        return binarySearchStudents(students, 0, students.length-1, lastName);
+    }
+    private static int binarySearchStudents(Student[] a, int low, int high, String lastName) {
+        // Partial code borrowed from Cai Horstmann's Big Java
+        if (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (a[mid].getLastName().compareTo(lastName) == 0) {
+                return mid;
+            }
+            else if (a[mid].getLastName().compareTo(lastName) < 0) {
+                return binarySearchStudents(a, mid + 1, high, lastName);
+            }
+            else {
+                return binarySearchStudents(a, low, mid - 1, lastName);
+            }
+        }
+        else {
+            return -1;
+        }
     }
 }
--- cs46b/homework/hw05final/src/Tester.java
+++ cs46b/homework/hw05final/src/Tester.java
@@ -1,5 +1,71 @@
-/**
- * Created by scot on 4/1/15.
- */
-public class Tester {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Tester
+{
+    public static void main(String[] args)
+    {
+        Student[] students = new Student[10];
+        students[0] = new Student("i", "i", 100, "i@gmail.com");
+        students[1] = new Student("lowest", "studentid", 1, "j@gmail.com");
+        students[2] = new Student("c", "c", 31, "c@gmail.com");
+        students[3] = new Student("h", "h", 234, "h@gmail.com");
+        students[4] = new Student("d", "d", 78, "d@gmail.com");
+        students[5] = new Student("g", "g", 72, "g@gmail.com");
+        students[6] = new Student("f", "f", 67, "f@gmail.com");
+        students[7] = new Student("b", "b", 783, "b@gmail.com");
+        students[8] = new Student("a", "a", 123, "a@gmail.com");
+        students[9] = new Student("e", "e", 54, "e@gmail.com");
+
+        Student[] sortedStudents = Sorter.mergeSortStudents(students);
+
+        System.out.println("Actual: " + sortedStudents[0]);
+        System.out.println("Expected: a a");
+        System.out.println("Actual: " + sortedStudents[3]);
+        System.out.println("Expected: d d");
+
+        Comparator<Student> studentIdComparator = Sorter.getStudentIdComparator();
+        Arrays.sort(sortedStudents, studentIdComparator);
+
+        System.out.println("Actual: " + sortedStudents[0]);
+        System.out.println("Expected: lowest studentid");
+
+        Course[] courses = new Course[2];
+        Course c1 = new Course("cs", 46, "Data Structures");
+        c1.addStudent(new Student("b", "b", 1, "b@gmail"));
+        Course c2 = new Course("mus", 101, "Intro to music theory");
+        c2.addStudents(students);
+        courses[0] = c2;
+        courses[1] = c1;
+
+        Sorter.selectionSortCourses(courses);
+
+        System.out.println("Actual: " + courses[0]);
+        System.out.println("Expected: cs-46: Data Structures");
+        System.out.println("Actual: " + courses[1]);
+        System.out.println("Expected: mus-101: Intro to music theory");
+
+        courses = new Course[4];
+        courses[0] = c1;
+        courses[1] = c2;
+        courses[2] = new Course("psy", 102, "Abnormal Psych");
+        courses[3] = new Course("eng", 154, "Mech Eng");
+
+        Comparator<Course> departmentComparator = Sorter.getDepartmentComparator();
+        Arrays.sort(courses, departmentComparator);
+
+        System.out.println("Actual: " + courses[1]);
+        System.out.println("Expected: eng-154: Mech Eng");
+
+        sortedStudents = Sorter.mergeSortStudents(students);
+        System.out.println("Actual: " + Sorter.binarySearchStudents("a", sortedStudents));
+        System.out.println("Expected: 0");
+        System.out.println("Actual: " + Sorter.binarySearchStudents("d", sortedStudents));
+        System.out.println("Expected: 3");
+        System.out.println("Actual: " + Sorter.binarySearchStudents("y", sortedStudents));
+        System.out.println("Expected: -1");
+
+    }
+
+
 }
