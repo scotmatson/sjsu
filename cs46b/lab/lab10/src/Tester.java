6aa2bd8729a2088c10f91df32a627cbc884ae066
Scot Matson <sjpmatson@gmail.com>
2015-04-11 17:28:14 -0700

    Completed up through part G of lab10

---
+++ cs46b/lab/lab10/src/Tester.java
@@ -0,0 +1,19 @@
+/**
+ * Created by scot on 4/10/15.
+ *
+ * File used to test the functionality of the zipup bash script
+ *  for sjsu cs46b lab 10
+ *
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+
+
+        // Arbitrary text output to text shell script
+        System.out.println("Testing bash shell script.");
+    }
+
+
+
+}
