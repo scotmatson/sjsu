5e5fc03f91cf4917b1f00f8e8a02018d9e5c4f75
Scot Matson <sjpmatson@gmail.com>
2015-06-05 12:23:52 -0700

    Init Test file for constructing, setting, and retrieving names. Added shortcut functionality for full names.

--- cs146/homework/src/assignment01/NameFinder.java
+++ cs146/homework/src/assignment01/NameFinder.java
@@ -3,7 +3,7 @@
 
  Class to store and locate instances of a given name.
 
- Solves CS 147 Homework Assignment #01
+ Solves CS147 Homework Assignment #01
 
  @author Scot Matson
  @version 1.00 2015/6/5
@@ -16,6 +16,12 @@ public class NameFinder
    private String firstName;
    private String lastName;
 
+   /**
+    Constructor for the name finder class.
+
+    @param first the first name.
+    @param last the last name.
+    */
    NameFinder (String first, String last)
    {
       firstName = first;
@@ -32,18 +38,54 @@ public class NameFinder
       return lastName;
    }
 
+   /**
+    Sets the last name.
+
+    @param lastName the last name.
+    */
    public void setLastName(String lastName)
    {
       this.lastName = lastName;
    }
 
+   /**
+    Gets the first name.
+
+    @return the first name.
+    */
    public String getFirstName()
    {
       return firstName;
    }
 
+   /**
+    Sets the first name.
+
+    @param firstName the first name.
+    */
    public void setFirstName(String firstName)
    {
       this.firstName = firstName;
+   }
+
+   /**
+    Constructs the full name by concatenating the firstName and lastName
+    instance variables.
+
+    @return the full name.
+    */
+   public String getFullName() {
+      return getFirstName() + " " + getLastName();
+   }
+
+   /**
+    Sets the first and last name.
+
+    @param first the first name.
+    @param last the last name.
+    */
+   public void setFullName(String first, String last) {
+      setFirstName(first);
+      setLastName(last);
    }
 }
--- cs146/homework/src/assignment01/NameFinderDemo.java
+++ cs146/homework/src/assignment01/NameFinderDemo.java
@@ -3,7 +3,7 @@
 
  Test class for the NameFinder.java class.
 
- Solves CS 147 Homework Assignment #01
+ Solves CS147 Homework Assignment #01
 
  @author Scot Matson
  @version 1.00 2015/6/5
@@ -20,6 +20,8 @@ public class NameFinderDemo
     {
        // URL to full text of War and Peace
        URL url = new URL("http://www.cs.sjsu.edu/~mak/CS146/assignments/1/WarAndPeace.txt");
+
+
     }
 
 }
