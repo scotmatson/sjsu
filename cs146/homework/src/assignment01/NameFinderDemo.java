cd223c8ea744b129c7493fec2a94df53e72e8bf3
Scot Matson <sjpmatson@gmail.com>
2015-06-16 20:59:16 -0700

    Added new lectures slides for cs146. Initiated assignment #2 template for cs146, completed assignment #1 for cs146.

--- cs146/homework/src/assignment01/NameFinderDemo.java
+++ cs146/homework/src/assignment01/NameFinderDemo.java
@@ -21,78 +21,81 @@ public class NameFinderDemo
 {
    public static void main (String[] args) throws IOException
    {
-      //double sumPerformanceTimes = 0;
-      //for (int i = 0; i < 100; ++i)
-      //{
-         // Performance testing start time.
-         double startTime = System.currentTimeMillis();
+      // Performance testing start time.
+      double startTime = System.currentTimeMillis();
 
-         // URL to to War and Peace text file.
-         URL warAndPeace = new URL("http://www.cs.sjsu.edu/~mak/CS146/assignments/1/WarAndPeace.txt");
+      // URL to to War and Peace text file.
+      URL warAndPeace = new URL("http://www.cs.sjsu.edu/~mak/CS146/assignments/1/WarAndPeace.txt");
 
-         // Names we are interested in finding.
-         Person[] people = {new Person("Makar", "Alexeevich"), new Person("Joseph", "Bazdeev"), new Person("Boris", "Drubetskoy")};
+      // Names we are interested in finding.
+      Person[] people = {
+         new Person("Makar", "Alexeevich"),
+         new Person("Joseph", "Bazdeev"),
+         new Person("Boris", "Drubetskoy")
+      };
 
-         // Reading data from URL
-         BufferedReader br = new BufferedReader(new InputStreamReader(warAndPeace.openStream()));
+      // Reading data from URL
+      BufferedReader br = new BufferedReader(new InputStreamReader(warAndPeace.openStream()));
 
-         // Setting up to output data.
-         PrintWriter pw = new PrintWriter("output.txt", "UTF-8");
+      // Setting up to output data.
+      PrintWriter pw = new PrintWriter("output.txt", "UTF-8");
 
-         // Building header.
-         pw.write(" LINE  POSITION  NAME\n");
+      // Building header.
+      pw.write(" LINE  POSITION  NAME\n");
 
-         // Preparing to parse.
-         Scanner stdin = new Scanner(br);
-         stdin.useDelimiter("[\\s,.!?';]");
+      // Preparing to parse.
+      Scanner stdin = new Scanner(br);
+      stdin.useDelimiter("[\\s,.!?';]");
 
-         int lineNumber = 0;
-         while (stdin.hasNextLine())
+      int lineNumber = 0;
+      while (stdin.hasNextLine()) {
+         String line = stdin.nextLine();
+
+         ++lineNumber;
+         String lastWord = line.substring(line.lastIndexOf(" ") + 1);
+
+         // MAKAR
+         if (line.contains(people[0].getFullName()))
          {
-            String line = stdin.nextLine();
-
-            ++lineNumber;
-            String lastWord = line.substring(line.lastIndexOf(" ") + 1);
-
-            // MAKAR
-            if (line.contains(people[0].getFullName()))
-            {
-               pw.format("%5d  %8d  %s\n", lineNumber, (1 + line.indexOf(people[0].getFullName())), people[0].getFullName());
-            }
-            if (lastWord.equals(people[0].getFirstName()) && stdin.hasNext(people[0].getLastName()))
-            {
-               pw.format("%5d  %8d  %s\n", lineNumber, (1 + line.indexOf(people[0].getFirstName())), people[0].getFullName());
-            }
-
-            // JOSEPH
-            if (line.contains(people[1].getFullName()))
-            {
-               pw.format("%5d  %8d  %s\n", lineNumber, (1 + line.indexOf(people[1].getFullName())), people[1].getFullName());
-            }
-            if (lastWord.equals(people[1].getFirstName()) && stdin.hasNext(people[1].getLastName()))
-            {
-               pw.format("%5d  %8d  %s\n", lineNumber, (1 + line.indexOf(people[1].getFirstName())), people[1].getFullName());
-            }
-
-            // BORIS
-            if (line.contains(people[2].getFullName()))
-            {
-               pw.format("%5d  %8d  %s\n", lineNumber, (1 + line.indexOf(people[2].getFullName())), people[2].getFullName());
-            }
-            if (lastWord.equals(people[2].getFirstName()) && stdin.hasNext(people[2].getLastName()))
-            {
-               pw.format("%5d  %8d  %s\n", lineNumber, (1 + line.indexOf(people[2].getFirstName())), people[2].getFullName());
-            }
+            pw.format("%5d  %8d  %s\n", lineNumber,
+               (1 + line.indexOf(people[0].getFullName())), people[0].getFullName());
+         }
+         if (lastWord.equals(people[0].getFirstName()) && stdin.hasNext(people[0].getLastName()))
+         {
+            pw.format("%5d  %8d  %s\n", lineNumber,
+               (1 + line.indexOf(people[0].getFirstName())), people[0].getFullName());
          }
 
-         // Performance testing end time.
-         double endTime = System.currentTimeMillis();
-         pw.write("\nTotal Run Time: " + (endTime - startTime) + " millisecond(s)\n");
+         // JOSEPH
+         if (line.contains(people[1].getFullName()))
+         {
+            pw.format("%5d  %8d  %s\n", lineNumber,
+               (1 + line.indexOf(people[1].getFullName())), people[1].getFullName());
+         }
+         if (lastWord.equals(people[1].getFirstName()) && stdin.hasNext(people[1].getLastName()))
+         {
+            pw.format("%5d  %8d  %s\n", lineNumber,
+               (1 + line.indexOf(people[1].getFirstName())), people[1].getFullName());
+         }
 
-         pw.close();
-         //sumPerformanceTimes += (endTime - startTime );
-      //}
-      //System.out.println("Average Performance Time: " + sumPerformanceTimes / 100);
+         // BORIS
+         if (line.contains(people[2].getFullName()))
+         {
+            pw.format("%5d  %8d  %s\n", lineNumber,
+               (1 + line.indexOf(people[2].getFullName())), people[2].getFullName());
+         }
+         if (lastWord.equals(people[2].getFirstName()) && stdin.hasNext(people[2].getLastName()))
+         {
+            pw.format("%5d  %8d  %s\n", lineNumber,
+               (1 + line.indexOf(people[2].getFirstName())), people[2].getFullName());
+         }
+      }
+
+      // Performance testing end time.
+      double endTime = System.currentTimeMillis();
+      pw.write("\nTotal Run Time: " + (endTime - startTime) + " millisecond(s)\n");
+
+      pw.close();
    }
 //
 }
---
+++ cs146/homework/src/assignment02/IndexedList.java
@@ -0,0 +1,18 @@
+/**
+ COPYRIGHT (C) 2015 Scot Matson. All Rights Reserved
+
+ Implementation of a List type which optimizes accessing, retrieving, insertion,
+   and deletion by interweaving a LinkedList with an ArrayList.
+
+ Solves CS 146 homework assignmen #2
+
+ @author Scot Matson
+
+ @version 1.00 2015/6/16
+ */
+package assignment02;
+
+public class IndexedList
+{
+
+}
