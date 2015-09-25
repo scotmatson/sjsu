6017be8e947b25846eb118027df0855f0c0cd954
Scot Matson <sjpmatson@gmail.com>
2015-09-25 14:52:45 -0700

    calendar app framework built out more. Working on display

--- cs151/assn02/CalendarApp/src/MyCalendar.java
+++ cs151/assn02/CalendarApp/src/MyCalendar.java
@@ -1,11 +1,18 @@
 /**
  * 
  */
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
+enum State {
+   ACTIVE,
+   INACTIVE,
+   EXIT,
+   ERR
+}
 public class MyCalendar
 { 
-   AppState status = AppState.ACTIVE;
+   State status = State.ACTIVE;
    Scanner in = new Scanner(System.in);
    
    public void printHeader()
@@ -21,36 +28,95 @@ public class MyCalendar
       System.out.printf("%s\n\n", "###################################");
    }
    
-   public void processResponse(char c)
+   /*
+    * Displays main menu options
+    * and prompts user to make a selection.
+    * 
+    * @return selection result.
+    */
+   public char printMenu()
    {
-      switch(c)
-      {
+      System.out.printf("%s\n", "Please make a selection from the following list of options.");
+      System.out.printf("%s, %s, %s, %s, %s, %s, %s, %s\n", 
+            "[L]oad",
+            "[V]iew by",
+            "[C]reate",
+            "[G]o to",
+            "[E]vent list",
+            "[D]elete",
+            "[Q]uit");
       
+      return parseInput();
+   }
+   
+   public void processEvent(char c, Planner p)
+   {
+      switch (Character.toLowerCase(c))
+      {
+         case 'l':
+            p.loadEvents();
+            break;
+         case 'v':
+            p.toggleView();
+            break;
+         case 'c':
+            //p.createEvent(date, title, description);
+            break;
+         case 'g':
+            p.goToEvent();
+            break;
+         case 'e':
+            p.listEvents();
+            break;
+         case 'd':
+            p.deleteEvent();
+            break;
+         case 'q':
+            status = AppState.EXIT;
+            break;
+         default:
+            break;
       }
+   }
+   
+   /**
+    * Parses console input and returns the first
+    * character.
+    * 
+    * @return user input.
+    */
+   public char parseInput()
+   {
+      char ch;
+      // Receive input.
+      System.out.printf(">> ");
+      ch = in.findInLine(".").charAt(0);
+      
+      // Flush any additional input.
+      in.nextLine();
+      
+      // Display user selection.
+      System.out.printf("%c\n\n", ch);
+      
+      return ch;
    }
    
    /**
     *    
     */
    public void run()
-   {
-      Menu main = new Menu(in);
-      char selection;  
+   {     
+      GregorianCalendar gc = new GregorianCalendar();
+      Planner p = new Planner("events.txt", gc);
+      char action;
       
-      while (status != AppState.EXIT) {
+      while (status != AppState.EXIT) {   
          
-         // Print menu options.
-         main.printMenu();
-         
-         // Recieve input.
-         System.out.printf(">> ");
-         selection = in.findInLine(".").charAt(0);
-         System.out.printf("%c\n\n", selection);
-         
-         // Parse selection
-         processMenuSelection();
-         
+         // Display Calendar
+         action = printMenu();
+         processEvent(action, p);
       }
+      
       exit();
    }
    
@@ -59,6 +125,7 @@ public class MyCalendar
     */
    public void exit()
    {
+      in.close();
       System.exit(0);
    }
    
