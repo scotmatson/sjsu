cb9b4af2673288206062c5e967eaaa9d9dabfb9a
Scot Matson <sjpmatson@gmail.com>
2015-09-26 00:31:24 -0700

    Semi working output. Working on storing data now.

--- cs151/assn02/CalendarApp/src/Event.java
+++ cs151/assn02/CalendarApp/src/Event.java
@@ -6,16 +6,19 @@ public class Event
 {
    private Date date; // This will be most important, can use as a reference for other information.
    private String title;
-   private String description;
+   private int start;
+   private int end;
+   
    // Should store an event
       // Date
       // Description
       // Title
    
-   public Event(Date date, String title, String description)
+   public Event(Date date, int start, int end, String title)
    {
       this.date = date;
+      this.start = start;
+      this.end = end;
       this.title = title;
-      this.description = description;
    }
 }
